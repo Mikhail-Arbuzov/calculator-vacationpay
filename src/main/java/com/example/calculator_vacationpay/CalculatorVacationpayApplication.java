8353f2ce52c92489f79ebf73407325649e14a4f3
mihan <mihan-arbuzov@yandex.ru>
2022-10-15 16:32:01 +0400

    Создал проект calculator_vacationpay

---
+++ src/main/java/com/example/calculator_vacationpay/CalculatorVacationpayApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculator_vacationpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorVacationpayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorVacationpayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/calculator_vacationpay/CalculatorVacationpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculator_vacationpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorVacationpayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
