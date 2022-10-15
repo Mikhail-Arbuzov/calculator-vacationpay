ee7f2092c4d222916f27a56799c7ffcd7f0190b1
mihan <mihan-arbuzov@yandex.ru>
2022-10-15 18:36:04 +0400

    Добавил комментарий в класс CreatorCalculateSumVacationPay для пояснения

--- src/main/java/com/example/calculator_vacationpay/service/creators/CreatorCalculateSumVacationPay.java
+++ src/main/java/com/example/calculator_vacationpay/service/creators/CreatorCalculateSumVacationPay.java
@@ -3,6 +3,7 @@ package com.example.calculator_vacationpay.service.creators;
 import com.example.calculator_vacationpay.service.calculation.CalculatorSumVacationPay;
 import com.example.calculator_vacationpay.service.calculation.ICalculateSumVacationPay;
 
+//Класс для создания и инициализации объектов реализующих интерфейс ICalculateSumVacationPay
 public class CreatorCalculateSumVacationPay implements ICreatorCalculateSumVacationPay {
     @Override
     public ICalculateSumVacationPay createCalculateSumVacationPay() {
